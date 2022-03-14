690048295285aafa05df959303fcfeefdd215202
s2121409, Rhianna Betia <99110677+RH14NN4B3T14@users.noreply.github.com>
2022-03-14 17:31:57 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,10 +4,11 @@
     <title> My Home Page </title>
    </head>
    <body>
-     <h1> Welcome to my Website! My name is Rhianna P. Betia </h1>
+     <h1> Welcome to my Website! My name is Rhianna P. Betia ♡ </h1>
     <ul>
       <li><a href= "index.php" > HOME <a/></li>
       <li><a href= "/LT2/index.html" > LEARNING TASK 2 <a/></li>
+      <li><a href= "/LT3/index.html" > LEARNING TASK 3 <a/></li>
      </ul>
     </body>
 </html>
